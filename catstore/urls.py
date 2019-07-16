d874757d9aca45fd30655bec69150decc363ea94
Jonny1987 <jrshannie@yahoo.com>
2019-07-16 17:16:09 +0100

    Fixed admin not working after adding namespace to store

--- catstore/urls.py
+++ catstore/urls.py
@@ -24,5 +24,5 @@ from store import api_views
 
 urlpatterns = [
     path('', include('store.urls')),
-    # path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/urls.py
+++ store/urls.py
@@ -14,7 +14,6 @@ router.register('products', ProductViewSet)
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
-    path('admin/', admin.site.urls),
     path('', views.HomeView.as_view(), name='home'),
     path('product/<int:pk>/', views.ProductView.as_view(), name='product'),
     path('request/', views.request_product, name='request'),
