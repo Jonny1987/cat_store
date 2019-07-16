8c8a9363996f1a5a643ee06579ca80474324ed45
Jonny1987 <jrshannie@yahoo.com>
2019-07-16 17:18:10 +0100

    Added tests for product view

--- store/tests.py
+++ store/tests.py
@@ -20,3 +20,19 @@ class StoreHomeViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context['products'], [repr(prod)])
 
+
+class StoreProductViewTests(TestCase):
+    def test_product_id_not_exist(self):
+        url = reverse('store:product', args=[1])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_product_id_exists(self):
+        cat = Category.objects.create(name='test')
+        cat.save()
+        prod = Product.objects.create(name='test', description='test', price=1, category=cat)
+        prod.save()
+        url = reverse('store:product', args=[1])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['product'], prod)
