cc2d2c95ad9b0ed8a7c0c63872efd9db064f79bd
Jonny1987 <jrshannie@yahoo.com>
2019-07-16 14:51:14 +0100

    Created tests for products on homepage

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from store.models import Product, Category
+
+
+class StoreHomeViewTests(TestCase):
+    def test_no_products(self):
+        response = self.client.get(reverse('store:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['products'], [])
+
+    def test_one_product(self):
+        cat = Category.objects.create(name='test')
+        cat.save()
+        prod = Product.objects.create(name='test', description='test', price=1, category=cat)
+        prod.save()
+        url = reverse('store:home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['products'], [repr(prod)])
+
